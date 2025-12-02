9717c9e5d9a94f4b14b27b5853d5f2a009bad03a
Akshobhya-1012 <akshobhyabellurkar1@gmail.com>
2025-12-02 11:49:01 +0530

    Added gitignore file

---
+++ java/src/test/java/com/data_structures/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data_structures.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
